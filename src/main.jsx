ebbebe93f7c04b29e991ebb5607ec8afe9f62b85
AhmedOsama143 <ao8911323@gmail.com>
2025-10-03 06:01:05 +0300

    fix-errors

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { store } from "./store";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter basename="/simple-E-Commerce/">
+      <HashRouter>
         <App />
       </HashRouter>
     </Provider>
