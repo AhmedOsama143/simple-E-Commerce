a4f872c90ad4c0e1bdf7a32cce7fe2cabbcdebb6
AhmedOsama143 <ao8911323@gmail.com>
2025-10-03 05:31:01 +0300

    commit-2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -12,9 +12,9 @@ import { store } from "./store";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Start-Framework/">
+      <HashRouter basename="/Start-Framework/">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
